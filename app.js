25ce0c07a1311ff04c51d766eab98a1dddb1b6d4
matcha1290 <masakura20071110@gmail.com>
2026-07-23 18:23:54 +0900

    Ver0.3 メニュー動作修正

--- app.js
+++ app.js
@@ -1,21 +1,76 @@
 const map = L.map('map').setView([35.681236, 139.767125], 6);
 
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+
+// 現在地表示
 if (navigator.geolocation) {
+
     navigator.geolocation.getCurrentPosition(
         (position) => {
+
             const lat = position.coords.latitude;
             const lng = position.coords.longitude;
+
 
             L.marker([lat, lng])
                 .addTo(map)
                 .bindPopup("📍現在地")
                 .openPopup();
 
+
             map.setView([lat, lng], 15);
+
         }
     );
+
 }
+
+
+
+// 下部メニュー
+
+const navItems = document.querySelectorAll(".nav-item");
+
+
+navItems.forEach(item => {
+
+    item.addEventListener("click", () => {
+
+        const page = item.dataset.page;
+
+
+        if (page === "map") {
+            alert("地図を表示します");
+        }
+
+
+        if (page === "search") {
+            alert("検索機能は準備中です");
+        }
+
+
+        if (page === "favorite") {
+            alert("お気に入り機能は準備中です");
+        }
+
+
+        if (page === "stats") {
+            alert("統計機能は準備中です");
+        }
+
+
+        if (page === "settings") {
+            alert("設定画面は準備中です");
+        }
+
+    });
+
+});
+
+
+console.log("Starbucks Map 起動");
+alert("app.js確認");
